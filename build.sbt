b378a8379b3eddd1d5c3b892b37c4c4d771874af
Arpan Chaudhury <Arpan.Chaudhury@agoda.com>
2017-06-10 14:40:28 +0700

    remove SQLite as SP not suported

--- build.sbt
+++ build.sbt
@@ -1,17 +1,14 @@
 import sbt._
 import sbt.Keys._
 
-lazy val commonSettings = Seq(
-  version := "0.9.0.0",
-  organization := "com.agoda",
-  scalaVersion := "2.12.1"
-)
-
 lazy val `kafka-jdbc-connector` =
   (project in file("."))
     .settings(
       name := "kafka-jdbc-connector",
-      commonSettings,
+      version := "0.9.0.0",
+      organization := "com.agoda",
+      scalaVersion := "2.11.7",
+      crossScalaVersions := Seq("2.11.7", "2.12.2"),
       libraryDependencies ++= Dependencies.Compile.kafkaJdbcConnector ++ Dependencies.Test.kafkaJdbcConnector
     )
     .enablePlugins(BuildInfoPlugin)
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,23 +1,21 @@
 import sbt._
 
 object Dependencies {
-  private val KafkaV         = "0.9.0.0"
-  private val ScalaTestV     = "3.0.1"
+  private val KafkaV     = "0.9.0.0"
+  private val ScalaTestV = "3.0.1"
 
-  private val ScalaLogging     = "com.typesafe.scala-logging" %% "scala-logging"        % "3.5.0"
-  private val MysqlDriver      = "mysql"                       % "mysql-connector-java" % "6.0.6"
-  private val MssqlDriver      = "com.microsoft.sqlserver"     % "mssql-jdbc"           % "6.1.0.jre8" exclude("javax.xml.stream", "stax-api")
-  private val SqliteDriver     = "org.xerial"                  % "sqlite-jdbc"          % "3.16.1"
-  private val PostgresqlDriver = "org.postgresql"              % "postgresql"           % "42.0.0"
-  private val KafkaConnectApi  = "org.apache.kafka"            % "connect-api"          % KafkaV
-  private val Enumeratum       = "com.beachape"               %% "enumeratum"           % "1.5.12"
-  private val Scalatics        = "org.scalactic"              %% "scalactic"            % ScalaTestV   % "test"
-  private val ScalaTest        = "org.scalatest"              %% "scalatest"            % ScalaTestV   % "test"
+  private val ScalaLogging        = "com.typesafe.scala-logging" %% "scala-logging"        % "3.5.0"
+  private val MysqlDriver         = "mysql"                       % "mysql-connector-java" % "6.0.6"
+  private val MssqlDriver         = "com.microsoft.sqlserver"     % "mssql-jdbc"           % "6.1.0.jre8" exclude("javax.xml.stream", "stax-api")
+  private val PostgresqlDriver    = "org.postgresql"              % "postgresql"           % "42.0.0"
+  private val KafkaConnectApi     = "org.apache.kafka"            % "connect-api"          % KafkaV
+  private val Enumeratum          = "com.beachape"               %% "enumeratum"           % "1.5.12"
+  private val Scalatics           = "org.scalactic"              %% "scalactic"            % ScalaTestV  % "test"
+  private val ScalaTest           = "org.scalatest"              %% "scalatest"            % ScalaTestV  % "test"
 
   object Compile {
     def kafkaJdbcConnector  = Seq(
-      KafkaConnectApi, MysqlDriver, MssqlDriver, SqliteDriver,
-      PostgresqlDriver, Enumeratum, ScalaLogging
+      ScalaLogging, KafkaConnectApi, MysqlDriver, MssqlDriver, PostgresqlDriver, Enumeratum
     )
   }
 
